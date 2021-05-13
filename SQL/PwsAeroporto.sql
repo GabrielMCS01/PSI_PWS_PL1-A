8321f978273ed81fdbb4ae9fafc2b542370330cf
Goncalo <2201131@my.ipleiria.pt>
2021-05-13 13:41:24 +0100

    SQL
    db

---
+++ SQL/PwsAeroporto.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE PwsAeroporto;
+USE PwsAeroporto;
+
+Create table if not exists Utilizadores(
+	IdUtilizador INT UNSIGNED AUTO_INCREMENT,
+    NomeComleto VARCHAR(100) NOT NULL,
+    DataNascimento Date NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	Telefone VARCHAR(20) NOT NULL,
+    Utilizador VARCHAR(100) NOT NULL,
+    PasswordUtilizador VARCHAR(100) NOT NULL,
+	CONSTRAINT pk_Utilizadores_IdUtilizador PRIMARY KEY(IdUtilizador)
+)ENGINE=InnoDB;
+
+Create table if not exists Aeroportos(
+	IdAeroporto INT UNSIGNED AUTO_INCREMENT,
+    NomeAeroporto varchar(100) not null,
+    Pais varchar(100) not null,
+    Cidade varchar(100) not null,
+	CONSTRAINT pk_Aeroportos_IdVoo PRIMARY KEY(IdAeroporto)
+)ENGINE=InnoDB;
+
+Create table if not exists Avioes(
+	IdAviao INT UNSIGNED AUTO_INCREMENT,
+    NomeAviao varchar(100) not null,
+    Transportadora varchar(100) not null,
+	CONSTRAINT pk_Avioes_IdAviao PRIMARY KEY(IdAviao)
+)ENGINE=InnoDB;
+
+Create table if not exists Voos(
+	IdVoo INT UNSIGNED AUTO_INCREMENT,
+    DataHoraPartida datetime not null,
+    DataHoraChegada datetime not null,
+    IdAeroportoOrigem int not null,
+	IdAeroportoDestino int not null,
+    IdVooEscala int,
+    IdAviao int not null,
+	CONSTRAINT pk_Voos_IdVoo PRIMARY KEY(IdVoo),
+	CONSTRAINT fk_Voos_IdAeroportoOrigem FOREIGN KEY(IdAeroportoOrigem) REFERENCES Aeroportos(IdAeroporto),
+	CONSTRAINT fk_Voos_IdAeroportoDestino FOREIGN KEY(IdAeroportoDestino) REFERENCES Aeroportos(IdAeroporto),
+	CONSTRAINT fk_Voos_IdVooEscala FOREIGN KEY(IdVooEscala) REFERENCES Voos(IdVoo),
+    CONSTRAINT fk_Voos_IdAviao FOREIGN KEY(IdAviao) REFERENCES Avioes(IdAviao)
+)ENGINE=InnoDB;
+
+Create table if not exists ComprasVoo(
+IdCompraVoo INT UNSIGNED AUTO_INCREMENT,
+IdCliente int not null,
+IdVoo int not null,
+CONSTRAINT pk_ComprasVoo_IdCompraVoo PRIMARY KEY(IdCompraVoo),
+CONSTRAINT fk_ComprasVoo_IdCliente FOREIGN KEY(IdCliente) REFERENCES Utilizadores(IdUtilizador),
+CONSTRAINT fk_ComprasVoo_IdVoo FOREIGN KEY(IdVoo) REFERENCES Voos(IdVoo)
+)ENGINE=InnoDB;
