0f2196b29f9dfbbb70d43e0b0109a08615329e81
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-05-14 13:45:56 +0100

    ligação à base de dados e melhoramento do login

--- webapp/startup/config/db.php
+++ webapp/startup/config/db.php
@@ -4,9 +4,9 @@ $defaultDbConnection = array (
     'DBMS'          => 'mysql',
     'SERVER'        => 'localhost',
     'PORT'          => '3306',
-    'DATABASENAME'  => 'webapp',
-    'USER'          => 'root',
-    'PASSWORD'      => '',
+    'DATABASENAME'  => 'pwsaeroporto',
+    'USER'          => 'pwa',
+    'PASSWORD'      => 'pwa',
     'CHARSET'       => 'utf8',
     'COLLATION'     => 'utf8_unicode_ci',
 );
