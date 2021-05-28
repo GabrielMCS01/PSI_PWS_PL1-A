31104f323daa8882535b111fe328ade85d4822c5
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-05-28 13:09:48 +0100

    alteração no modelo scale

--- webapp/model/Scale.php
+++ webapp/model/Scale.php
@@ -4,11 +4,10 @@ use ActiveRecord\Model;
 
 class Scale extends Model{
 
-    // Uma "Escala" pertence a um "Avião", "Aeroporto de Origem", "Aeroporto de Chegada" e a um "Voo"
+    // Uma "Escala" pertence a um "Avião", "Aeroporto" e a um "Voo"
     static $belongs_to = [
         ['airplane'],
-        ['origin_airport', 'foreign_key' => 'originairport_id', 'class_name' => 'Airport'],
-        ['destination_airport', 'foreign_key' => 'destinationairport_id', 'class_name' => 'Airport'],
+        ['airport'],
         ['flight']
     ];
 }
