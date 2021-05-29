92f9e37e981567c3f35f34a42df6d15e60a89c97
Goncalo <2201131@my.ipleiria.pt>
2021-05-29 12:18:08 +0100

    Update db.php

--- webapp/startup/config/db.php
+++ webapp/startup/config/db.php
@@ -1,6 +1,8 @@
 <?php
 //Configuração do user e das portas do servidor MySql
 //Para esse efeito criamos um user no workbench e é a partir desse use "pwa" que o projeto acede à BD
+//Configuração da base de dados a ser utilizada
+//configurações gerais do servidor
 $defaultDbConnection = array (
     'DBMS'          => 'mysql',
     'SERVER'        => 'localhost',
