f4392ad894aaae4e5bb4a8fea939c4e1b5a24e4a
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-06-11 18:14:10 +0100

    icons

--- public/js/script.js
+++ public/js/script.js
@@ -7,12 +7,13 @@ function VooVolta(){
     if(document.querySelector('.voovolta').style.display == 'none'){
         $('.voovolta').show();
         if(document.querySelector('#exibirPesquisa') != null) {
-            document.querySelector('#exibirPesquisa').style.transform = 'rotate(180deg)';
+            console.log("ola");
+            document.querySelector('#setabaixo').setAttribute('d','M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z');
         }
     }else{
         $('.voovolta').hide();
         if(document.querySelector('#exibirPesquisa') != null) {
-            document.querySelector('#exibirPesquisa').style.transform = 'rotate(0deg)';
+            document.querySelector('#setabaixo').setAttribute('d', 'M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z');
         }
     }
 }
