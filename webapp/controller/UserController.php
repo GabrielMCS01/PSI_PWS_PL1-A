48053c59e549f04b3f479cde576744b67d7dfbb4
Diogo Rafael Cunha Pereira <diogo.rc.pereira@gmail.com>
2021-06-13 00:55:12 +0100

    Encriptação da password

--- webapp/controller/UserController.php
+++ webapp/controller/UserController.php
@@ -12,6 +12,17 @@ class UserController extends BaseController implements ResourceControllerInterfa
     //Função que retorna a página index do user(Login)
     public function index()
     {
+        // Encripta a password do administrador a primeira vez que abre o programa
+        $numeroUsers = User::count();
+        if($numeroUsers == 1){
+            $user = User::first();
+            if($user->userpassword == 'adminmaster') {
+                $hash = password_hash($user->userpassword,
+                    PASSWORD_DEFAULT);
+                $user->update_attributes(['userpassword' => $hash]);
+                $user->save();
+            }
+        }
         return View::make('users.index');
     }
 
@@ -25,13 +36,15 @@ class UserController extends BaseController implements ResourceControllerInterfa
     public function store()
     {
         ActiveRecord\Connection::$datetime_format = 'Y-m-d H:i:s';
+        $hash = password_hash(Post::get('userpassword'),
+            PASSWORD_DEFAULT);
         $dados = [
             'username' => Post::get('username'),
             'fullname' => Post::get('fullname'),
             'birthdate' => Post::get('birthdate'),
             'email' => Post::get('email'),
             'phonenumber' => Post::get('phonenumber'),
-            'userpassword' => Post::get('userpassword'),
+            'userpassword' => $hash,
             'userprofile' => 'passageiro'
             ];
         $utilizador = new User($dados);
@@ -115,35 +128,43 @@ class UserController extends BaseController implements ResourceControllerInterfa
     public function login($id)
     {
         if($id == 'logged') {
-            $utilizadores = User::first(Post::getAll());
+            // Pesquisa se existe um utilizador com este nome
+            $utilizadores = User::first(['username' => Post::get('username')]);
             if ($utilizadores != null) {
-                // Armazenar o nome do utilizador, o id e o seu tipo perfil(admin/passageiro...etc) em varivaeis super globais(Session)
-                $_SESSION['username'] = Post::get('username');
-                $_SESSION['userid'] = $utilizadores->users_id;
-                $_SESSION['tipoUser'] = $utilizadores->userprofile;
-                switch ($_SESSION['tipoUser']) {
-                    case "passageiro":
-                        Redirect::toRoute('flights/index');
-                        break;
-                    case "administrador":
-                        Redirect::toRoute('airports/index');
-                        break;
-                    case "gestordevoo":
-                        Redirect::toRoute('flights/index');
-                        break;
-                    case "operadordecheckin":
-                        Redirect::toRoute('users_flights/index');
-                        break;
+
+                // Verifica se a password inserida corresponde com a password da base de dados
+                $verify = password_verify(Post::get('userpassword'), $utilizadores->userpassword);
+
+                if ($verify) {
+                    // Armazenar o nome do utilizador, o id e o seu tipo perfil(admin/passageiro...etc) em varivaeis super globais(Session)
+                    $_SESSION['username'] = Post::get('username');
+                    $_SESSION['userid'] = $utilizadores->users_id;
+                    $_SESSION['tipoUser'] = $utilizadores->userprofile;
+
+                    switch ($_SESSION['tipoUser']) {
+                        case "passageiro":
+                            Redirect::toRoute('flights/index');
+                            break;
+                        case "administrador":
+                            Redirect::toRoute('airports/index');
+                            break;
+                        case "gestordevoo":
+                            Redirect::toRoute('flights/index');
+                            break;
+                        case "operadordecheckin":
+                            Redirect::toRoute('users_flights/index');
+                            break;
+                    }
+                } else {
+                    session_unset();
+                    $_SERVER['mensagem'] = "Username ou Password incorreto";
+                    return View::make('users.index');
                 }
             } else {
                 session_unset();
                 $_SERVER['mensagem'] = "Username ou Password incorreto";
                 return View::make('users.index');
             }
-        }else{
-            session_unset();
-            $_SERVER['mensagem'] = "Username ou Password incorreto";
-            return View::make('users.index');
         }
     }
 
